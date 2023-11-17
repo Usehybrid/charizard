82a8e7246480fc4ea178c21bf90db8b67d9b8e61
Soham Sarkar <soham.ss1@gmail.com>
2023-11-17 12:16:17 +0530

    feat: empty state ui improvements

--- src/components/empty-state/EmptyState.tsx
+++ src/components/empty-state/EmptyState.tsx
@@ -7,7 +7,7 @@ import {SVG} from '../svg'
 export type EmptyStateProps = {
   icon: string
   title: string
-  desc: string
+  desc?: string
   flexDir?: 'row' | 'column'
   btnText?: string
   onClick?: React.MouseEventHandler<HTMLButtonElement>
@@ -26,12 +26,12 @@ export function EmptyState({
 }: EmptyStateProps) {
   return (
     <div className={clsx(classes.container, flexDir === 'row' && classes.containerRow)}>
-      <div className={classes.huiEmptyStateIconContainer}>
-        <SVG path={icon} />
+      <div className={classes.iconBox}>
+        <SVG path={icon} svgClassName={classes.icon} />
       </div>
       <div className={classes.info}>
         <h6 className={clsx(classes.title, flexDir === 'row' && classes.textRow)}>{title}</h6>
-        <p className={clsx(classes.desc, flexDir === 'row' && classes.textRow)}>{desc}</p>
+        {desc && <p className={clsx(classes.desc, flexDir === 'row' && classes.textRow)}>{desc}</p>}
       </div>
 
       {btnText && (
