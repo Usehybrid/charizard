f07c54c0465389322a1c967b2ff60e28aa64bb73
Soham Sarkar <soham.ss1@gmail.com>
2024-01-22 17:54:48 +0530

    table filter drawer

---
+++ src/components/table-v2/table-filters-drawer/TableFiltersDrawer.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import * as dialog from '@zag-js/dialog'
+import clsx from 'clsx'
+import filterIcon from '../../assets/filter-lines.svg'
+import classes from './table-filters-drawer.module.css'
+import {useMachine, normalizeProps, Portal} from '@zag-js/react'
+import {SVG} from '../../svg'
+
+interface TableFiltersDrawerProps {
+  a?: any
+}
+
+export default function TableFiltersDrawer({a}: TableFiltersDrawerProps) {
+  const [state, send] = useMachine(dialog.machine({id: 'charizard-table-filters'}))
+
+  const api = dialog.connect(state, send, normalizeProps)
+  return (
+    <>
+      <button
+        {...api.triggerProps}
+        className={clsx('hybr1d-ui-reset-btn', classes.actionCommon, classes.filterBtn)}
+      >
+        <SVG path={filterIcon} width={22} height={22} />
+        Filter
+      </button>
+      {api.isOpen && (
+        <Portal>
+          <div {...api.backdropProps} />
+          <div {...api.positionerProps}>
+            <div {...api.contentProps}>
+              <h2 {...api.titleProps}>Edit profile</h2>
+              <p {...api.descriptionProps}>
+                Make changes to your profile here. Click save when you are done.
+              </p>
+              <div>
+                <input placeholder="Enter name..." />
+                <button>Save</button>
+              </div>
+              <button {...api.closeTriggerProps}>Close</button>
+            </div>
+          </div>
+        </Portal>
+      )}
+    </>
+  )
+}
--- src/components/table-v2/table-meta-header/TableMetaHeader.tsx
+++ src/components/table-v2/table-meta-header/TableMetaHeader.tsx
@@ -3,9 +3,15 @@ import classes from './table-meta-header.module.css'
 import {TableV2Props} from '../TableV2'
 import TableSelectedActions from '../table-selected-actions'
 import {Search} from '../../search'
-import {pluralize} from '../../../utils/text'
 import TableHeaderFilters from '../table-header-filters'
+import viewColIcon from '../../assets/view-columns.svg'
+import fileDownloadIcon from '../../assets/file-download.svg'
+import filterIcon from '../../assets/filter-lines.svg'
+import clsx from 'clsx'
+import {pluralize} from '../../../utils/text'
 import {SVG} from '../../svg'
+import {useDisclosure} from '../../../utils/hooks/use-disclosure'
+import TableFiltersDrawer from '../table-filters-drawer/TableFiltersDrawer'
 
 interface TableMetaHeaderProps {
   rowSelectionConfig: TableV2Props['rowSelectionConfig']
@@ -24,6 +30,7 @@ export default function TableMetaHeader({
   rowSelection,
   setRowSelection,
 }: TableMetaHeaderProps) {
+  const {isOpen, onOpen, onClose} = useDisclosure()
   const hasRowActions = rowSelectionConfig?.actions && rowSelectionConfig.actions.length > 0
 
   const rowsSelected = Object.keys(rowSelection).length
@@ -45,42 +52,62 @@ export default function TableMetaHeader({
   console.log(headerFilter)
 
   return (
-    <div className={classes.box}>
-      <div className={classes.texts}>
-        <p className={classes.heading}>
-          {isRowSelected ? selectedText : totalText} {isRowSelected ? 'Selected' : ''}
-        </p>
-        {hasRowActions && (
-          <TableSelectedActions
-            rowSelectionConfig={rowSelectionConfig}
-            rowSelection={rowSelection}
-          />
-        )}
-      </div>
-
-      <div className={classes.actions}>
-        {typeof searchConfig === 'object' && (
-          <div className={classes.search}>
-            <Search
-              id="table-search"
-              search={searchConfig.search}
-              setSearch={searchConfig.setSearch}
-              placeholder={searchConfig.placeholder}
-              clearIconClearFn={() => setRowSelection({})}
+    <>
+      <div className={classes.box}>
+        <div className={classes.texts}>
+          <p className={classes.heading}>
+            {isRowSelected ? selectedText : totalText} {isRowSelected ? 'Selected' : ''}
+          </p>
+          {hasRowActions && (
+            <TableSelectedActions
+              rowSelectionConfig={rowSelectionConfig}
+              rowSelection={rowSelection}
             />
-          </div>
-        )}
+          )}
+        </div>
 
-        {typeof filterConfig === 'object' && !!headerFilter && (
-          <TableHeaderFilters filterConfig={filterConfig} filters={headerFilter} />
-        )}
+        <div className={classes.actions}>
+          {typeof searchConfig === 'object' && (
+            <div className={classes.search}>
+              <Search
+                id="table-search"
+                search={searchConfig.search}
+                setSearch={searchConfig.setSearch}
+                placeholder={searchConfig.placeholder}
+                clearIconClearFn={() => setRowSelection({})}
+              />
+            </div>
+          )}
 
-        {
-          // typeof customColumnConfig === 'object'
+          {typeof filterConfig === 'object' && !!headerFilter && (
+            <TableHeaderFilters filterConfig={filterConfig} filters={headerFilter} />
+          )}
 
-          <div className={classes.customCols}>{/* <SVG path="" /> */}</div>
-        }
+          {/* {typeof filterConfig === 'object' && (
+            <div className={clsx(classes.actionCommon, classes.filterBtn)} onClick={onOpen}>
+              <SVG path={filterIcon} width={22} height={22} />
+              Filter
+            </div>
+          )} */}
+
+          {typeof filterConfig === 'object' && <TableFiltersDrawer />}
+
+          {
+            // typeof customColumnConfig === 'object'
+
+            <div className={classes.actionCommon}>
+              <SVG path={viewColIcon} width={22} height={22} />
+            </div>
+          }
+
+          {
+            // typeof exportConfig === 'object'
+            <div className={classes.actionCommon}>
+              <SVG path={fileDownloadIcon} width={22} height={22} />
+            </div>
+          }
+        </div>
       </div>
-    </div>
+    </>
   )
 }
