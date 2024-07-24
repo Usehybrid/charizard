ae0ca751de5d69ff4ce25dfc1ed83632d76e957c
komlraj <kmlraj131996@gmail.com>
2024-07-24 16:21:53 +0530

    Create tabs component

---
+++ src/components/tabs/Tabs.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement } from "react"
+import clsx from "clsx"
+// import { TabProps } from "./Tab"
+import classes from './styles.module.css'
+
+interface TabsProps {
+	id: string
+	activeKey: string
+	onSelect: (kay: string) => void
+	customClassName?: string
+	customStyles?: React.CSSProperties
+	children: ReactElement[]
+}
+
+export const Tabs: React.FC<TabsProps> = ({
+	id,
+	activeKey,
+	onSelect,
+	children,
+	customClassName = '',
+	customStyles = {}
+}) => {
+
+	const handleTabChange = (key: string) => {
+		onSelect(key)
+	}
+	return (
+		<div id={id} className={clsx(classes.tabs, customClassName)} style={customStyles}>
+			<div className={classes.tabBar}>
+				{React.Children.map(children, (child: React.ReactElement, idx) => 
+					React.cloneElement(child, {
+						isActive: child.props.title === activeKey,
+						onClick: () => handleTabChange(child.props.title)
+					})
+				)}
+			</div>
+			<div className={classes.tabContent}>
+				{
+					React.Children.map(children, (child: React.ReactElement) => (
+						child.props.title === activeKey ? child.props.children : ''
+					))
+				}
+			</div>
+		</div>
+	);
+};
