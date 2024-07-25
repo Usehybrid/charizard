525d966e8a4f1e70206779c480248235ccba0230
AbhishekHybr1d <abhishek.kolge@hybr1d.io>
2024-07-25 16:53:27 +0530

    export type updated
    Signed-off-by: AbhishekHybr1d <abhishek.kolge@hybr1d.io>

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -3,6 +3,6 @@ import classes from './styles.module.css'
 
 type SkeletonProps = React.HTMLAttributes<HTMLDivElement>
 
-export default function Skeleton({className, ...props}: SkeletonProps) {
+export function Skeleton({className, ...props}: SkeletonProps) {
   return <span className={clsx(classes.container, className)} {...props} />
 }
