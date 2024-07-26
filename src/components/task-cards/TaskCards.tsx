f96efa2712ee594c7101135c6f1d9d5b080e3b5d
Soham Sarkar <soham.ss1@gmail.com>
2024-07-26 16:20:32 +0530

    minor

--- src/components/task-cards/TaskCards.tsx
+++ src/components/task-cards/TaskCards.tsx
@@ -1,7 +1,6 @@
 import Card from './components/card/Card'
 import Header from './components/header/Header'
-import classes from './styles.module.css'
-import {Key} from 'react'
+import classes from './task-cards.module.css'
 import {Task} from './types'
 
 interface TaskCardProps {
@@ -13,8 +12,8 @@ export function TaskCard({headers, data}: TaskCardProps) {
   return (
     <div className={classes.taskCard}>
       <Header headers={headers} />
-      {data.map((data: Task, i: Key) => (
-        <Card data={data} key={i} />
+      {data.map((data: Task, idx) => (
+        <Card data={data} key={idx} />
       ))}
     </div>
   )
