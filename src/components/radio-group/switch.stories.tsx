48d0e8d6a7f650278550a98b87f21e0c6a2826a6
bim240 <bimlendu357@gmail.com>
2024-07-31 12:59:55 +0530

    add new style for radio btn

---
+++ src/components/radio-group/switch.stories.tsx
@@ -0,0 +1,31 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import {RadioGroup} from './RadioGroup'
+
+const meta: Meta<typeof RadioGroup> = {
+  component: RadioGroup,
+}
+
+export default meta
+
+type Story = StoryObj<typeof RadioGroup>
+
+export const RadioGroupDefault: Story = {
+  args: {
+    radioHeading: 'RadioGroup',
+    disabled: true,
+    items: [
+      {
+        label: {heading: 'label 1'},
+        value: 'label 1',
+      },
+      {
+        label: {heading: 'label 2'},
+        value: 'label 2',
+      },
+      {
+        label: {heading: 'label 3'},
+        value: 'label 3',
+      },
+    ],
+  },
+}
