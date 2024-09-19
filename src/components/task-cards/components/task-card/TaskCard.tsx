8c450812707562a1a16c7f6706bde494cab52235
Sanket Sarkar <sanket.sarkar@zenadmin.ai>
2024-09-19 17:21:23 +0530

    feat/task-card-click-enhancements

--- src/components/task-cards/components/task-card/TaskCard.tsx
+++ src/components/task-cards/components/task-card/TaskCard.tsx
@@ -42,7 +42,6 @@ export default function TaskCard({
           return
         }
         if (data.module_reference === 'attendance') {
-          // @ts-ignore
           navigate(`/attendance/approve/${data.task_details_id}`)
           return
         }
@@ -86,7 +85,17 @@ export default function TaskCard({
   }
 
   return (
-    <div className={classes.card} onMouseLeave={hideActionHandler}>
+    <div
+      className={clsx(classes.card, data.task_details_id ? classes.pointerCard : null)}
+      onMouseLeave={hideActionHandler}
+      onClick={() => {
+        if (data.task_details_id) {
+          navigate(`/${data.module_reference}/${data.task_details_id}`, {
+            state: {source: location.pathname},
+          })
+        }
+      }}
+    >
       <div className={classes.taskSection}>
         <div className={clsx(classes.taskName, 'zap-content-semibold')}>{data.name}</div>
         <div className={clsx(classes.dateAndTime, 'zap-caption-medium')}>
