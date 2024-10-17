c65fd3825e2bf3eecd5db468acd8561745f7df1c
Sanket Sarkar <sanket.sarkar@zenadmin.ai>
2024-10-17 08:20:43 +0530

    fix/updated-color-for-default-status

--- src/components/pill/Pill.tsx
+++ src/components/pill/Pill.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import clsx from 'clsx'
 import classes from './pill.module.css'
-import {statusMap} from '../badge'
+import { BADGE_STATUS } from '../badge'
 
 export enum PILL_STATUS {
   DEFAULT = 'default',
@@ -15,6 +15,15 @@ export enum PILL_STATUS {
 interface PillProps {
   status?: PILL_STATUS
   children: React.ReactNode
+}
+
+const statusMap = {
+  [BADGE_STATUS.NEUTRAL]: {bg: 'var(--dark-d10)', color: 'var(--dark-d70)'},
+  [BADGE_STATUS.DEFAULT]: {bg: 'var(--p-p10)', color: 'var(--p-p50)'},
+  [BADGE_STATUS.POSITIVE]: {bg: 'var(--status-success-s10)', color: 'var(--status-success-s70)'},
+  [BADGE_STATUS.HIGHLIGHT]: {bg: 'var(--status-info-i10)', color: 'var(--status-info-i70)'},
+  [BADGE_STATUS.WARNING]: {bg: 'var(--status-warning-w10)', color: 'var(--status-warning-w70)'},
+  [BADGE_STATUS.NEGATIVE]: {bg: 'var(--status-error-e10)', color: 'var(--status-error-e70)'},
 }
 
 export function Pill({status = PILL_STATUS.DEFAULT, children}: PillProps) {
