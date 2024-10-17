fc43a79fdcebf47ff812dd2871702eacf1f1165d
Soham Sarkar <soham.ss1@gmail.com>
2024-10-17 12:21:50 +0530

    improve table empty state

--- src/components/table/table-empty/TableEmpty.tsx
+++ src/components/table/table-empty/TableEmpty.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import standardIllustration from './standard.svg'
-import classes from './styles.module.css'
+import classes from './table-empty.module.css'
 import {TableProps} from '../Table'
 import {BUTTON_V2_TYPE, BUTTON_V2_VARIANT, ButtonV2} from '../../button-v2'
 import {SVG} from '../../svg'
@@ -24,16 +24,16 @@ export default function TableEmpty({emptyStateConfig, visibleCols}: TableEmptyPr
       <tr>
         <td style={emptyStateConfig?.customStyle} colSpan={visibleCols}>
           <div className={classes.box}>
-            <img
-              className={classes.illustration}
-              src={standardIllustration}
-              alt="empty-state-illustrations"
-            />
-
+            <div className={classes.illustrationContainer}>
+              <img
+                className={classes.illustration}
+                src={standardIllustration}
+                alt="empty-state-illustrations"
+              />
+            </div>
             <div className={classes.infoBox}>
               <h3 className={clsx(classes.head, 'zap-lead-semibold')}>{heading}</h3>
               <p className={clsx(classes.info, 'zap-content-regular')}>{info}</p>
-
               {hasCTA && (
                 <ButtonV2
                   variant={BUTTON_V2_VARIANT.PRIMARY}
