4ed18f0fed75d279d7a2bc52f1ff96dc3136b794
Soham Sarkar <soham.ss1@gmail.com>
2025-01-24 11:34:42 +0530

    minor

--- src/components/table/table-custom-cols/TableCustomCols.tsx
+++ src/components/table/table-custom-cols/TableCustomCols.tsx
@@ -89,7 +89,7 @@ export default function TableCustomCols({
     const initialState = columns?.checked_state || []
     setCheckedState(initialState)
     configureTable(initialState)
-  }, [isPending, isError, variant, columns?.checked_state])
+  }, [columns?.checked_state])
 
   const draggableCols = checkedState.filter(c => c.checked)
   const nonDraggableCols = checkedState.filter(c => !c.checked)
