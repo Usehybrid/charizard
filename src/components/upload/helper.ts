a98197101b56618fe5e4d9b18475a7a7582bcf10
Sanket Sarkar <sanket.sarkar@zenadmin.ai>
2025-02-12 15:55:52 +0530

    chore:upload-header-format

--- src/components/upload/helper.ts
+++ src/components/upload/helper.ts
@@ -75,9 +75,14 @@ export function renderFileTypes(extensions: string): string {
   extensionList.forEach(extension => {
     const fileType = extensionMap[extension]
     if (fileType) {
-      fileTypesSet.add(fileType)
+      fileType.split(', ').forEach(type => fileTypesSet.add(type))
     }
   })
 
-  return Array.from(fileTypesSet).join(', ')
+  const fileTypes = Array.from(fileTypesSet)
+
+  if (fileTypes.length === 0) return ''
+  if (fileTypes.length === 1) return `${fileTypes[0]} only`
+
+  return `${fileTypes.slice(0, -1).join(', ')} and ${fileTypes[fileTypes.length - 1]}`
 }
