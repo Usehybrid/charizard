ebdc9e0bba6753908a7418703dfdc50b64509207
Rashmil Rajpoot <rashmil.rajpoot@coralmango.com>
2025-03-24 16:42:39 +0530

    rollback-changes

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -3,15 +3,16 @@ import * as accordion from '@zag-js/accordion'
 import clsx from 'clsx'
 import classes from './style.module.css'
 import {useMachine, normalizeProps} from '@zag-js/react'
-import {AccordionProps, CollapseProps, HeaderProps, ItemProps} from './types'
+import {AccordionContextValue, AccordionProps, CollapseProps, HeaderProps, ItemProps} from './types'
+import {create} from 'zustand'
 
-type AccordionContextValue = {
-  api: ReturnType<typeof accordion.connect> | null
-  activeEventKeys: string[]
-  toggleKey: (key: string) => void
-}
-
-const AccordionContext = React.createContext<AccordionContextValue | null>(null)
+export const useAccordionStore = create<AccordionContextValue>(set => ({
+  api: null as any,
+  state: null,
+  send: () => {},
+  activeEventKey: [],
+  setActiveEventKey: keys => set({activeEventKey: keys}),
+}))
 
 export const Accordion = ({
   children,
@@ -22,53 +23,61 @@ export const Accordion = ({
   isOpenAll = false,
   allEventKeys = [],
 }: AccordionProps) => {
-  const [activeEventKeys, setActiveEventKeys] = React.useState<string[]>(
-    isOpenAll ? allEventKeys.map(String) : defaultActiveKey ? [defaultActiveKey] : [],
-  )
-
   const service = useMachine(accordion.machine, {
+    id: defaultActiveKey as string,
     collapsible: true,
     defaultValue: defaultActiveKey ? [defaultActiveKey] : undefined,
-    value: activeEventKeys,
+    value: isOpenAll ? allEventKeys.map(String) : defaultActiveKey ? [defaultActiveKey] : [],
     multiple: isMulti || isOpenAll,
   })
 
   const api = accordion.connect(service, normalizeProps)
 
-  const toggleKey = (key: string) => {
-    setActiveEventKeys(prevKeys =>
-      prevKeys.includes(key) ? prevKeys.filter(k => k !== key) : [...prevKeys, key],
-    )
-  }
+  const storedActiveKeys = useAccordionStore(state => state.activeEventKey)
+
+  React.useEffect(() => {
+    useAccordionStore.setState({api, state: service.state, send: service.send})
+  }, [api, service.state, service.send])
+
+  React.useEffect(() => {
+    const activeKeys = service.context.get('value') || []
+
+    if (JSON.stringify(storedActiveKeys) === JSON.stringify(activeKeys)) {
+      return
+    }
+
+    useAccordionStore.getState().setActiveEventKey(activeKeys)
+  }, [service.context.get('value'), storedActiveKeys])
 
   return (
-    <AccordionContext.Provider value={{api, activeEventKeys, toggleKey}}>
-      <div {...api.getRootProps()} className={customClasses} style={customStyle}>
-        {children}
-      </div>
-    </AccordionContext.Provider>
+    <div {...api.getRootProps()} className={customClasses} style={customStyle}>
+      {children}
+    </div>
   )
 }
 
 Accordion.Item = ({eventKey, children}: ItemProps) => {
-  const context = React.useContext(AccordionContext)
-  if (!context) throw new Error('Accordion.Item must be used within an <Accordion>')
-
-  return <div {...context.api?.getItemProps({value: eventKey})}>{children}</div>
+  const api = useAccordionStore(state => state.api)
+  if (api) return <div {...api.getItemProps({value: eventKey})}>{children}</div>
 }
 
 Accordion.Header = ({eventKey, children, customClasses, customStyle}: HeaderProps) => {
-  const context = React.useContext(AccordionContext)
-  if (!context) throw new Error('Accordion.Header must be used within an <Accordion>')
-
-  const {api, toggleKey} = context
-  if (!api) return null
-
+  const api = useAccordionStore(state => state.api)
+  if (!api) return
+  const setActiveEventKey = useAccordionStore(state => state.setActiveEventKey)
   const {onClick, ...triggerProps} = api.getItemTriggerProps({value: eventKey})
 
   const handleClick = (e: React.MouseEvent) => {
     onClick(e)
-    toggleKey(eventKey)
+
+    const currentActiveKeys = useAccordionStore.getState().activeEventKey || []
+    const isActive = currentActiveKeys.includes(eventKey)
+
+    const newActiveKeys = isActive
+      ? currentActiveKeys.filter(key => key !== eventKey)
+      : [...currentActiveKeys, eventKey]
+
+    setActiveEventKey(newActiveKeys)
   }
 
   return (
@@ -85,10 +94,9 @@ Accordion.Header = ({eventKey, children, customClasses, customStyle}: HeaderProp
 }
 
 Accordion.Collapse = ({eventKey, children, customClasses, customStyle}: CollapseProps) => {
-  const context = React.useContext(AccordionContext)
-  if (!context) throw new Error('Accordion.Collapse must be used within an <Accordion>')
+  const activeKeys = useAccordionStore(state => state.activeEventKey)
 
-  const isOpen = context.activeEventKeys.includes(eventKey)
+  const isOpen = activeKeys.includes(eventKey)
 
   return (
     <div style={customStyle} className={customClasses} hidden={!isOpen}>
